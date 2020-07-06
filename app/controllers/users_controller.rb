fd4e45ae1255a69ac47613d94804d673202b155c
travisprol <travisjohnprol@gmail.com>
2020-07-06 13:51:24 -0400

    some of user show/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(user.id)
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name)
+    end
 end

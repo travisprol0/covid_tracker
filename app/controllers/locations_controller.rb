b2144c917f058dcfced1a37b145d90bc83adc50b
afrolambo <stephennoahgalvan@.com>
2020-07-07 16:17:22 -0400

    Added API

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,11 +11,45 @@ class LocationsController < ApplicationController
 
     private
 
+    def states_info
+        url = "https://covidtracking.com/api/states/info"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfStatesInfo = workable_hash
+    end 
+    
+    def states_daily
+        url = "https://covidtracking.com/api/states/daily"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfStatesDaily = workable_hash
+    end 
+
     def states_current #arguemnt?
         url = "https://covidtracking.com/api/states"
         restClientResponseObject = RestClient.get(url)
         jsonButItsAString = restClientResponseObject.body
         workable_hash = JSON.parse(jsonButItsAString)   
-        @arrayOfCurrentStates = workable_hash["items"]
+        @arrayOfCurrentStates = workable_hash
+
     end
+
+    def us_daily
+        url = "https://covidtracking.com/api/us/daily"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfUSDaily = workable_hash
+    end 
+
+    def us_current
+        url = "http://covidtracking.com/api/us"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfUSCurrent = workable_hash
+    end 
+
 end

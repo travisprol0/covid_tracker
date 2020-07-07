4f7a1f4a17f38f6374b42eef91e8c4791934fcf2
travisprol0 <travisjohnprol@gmail.com>
2020-07-07 13:56:43 -0400

    API beginnings

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,21 @@
 class LocationsController < ApplicationController
     def index 
-        @locations = Location.all 
+        # @locations = Location.all
+        location = states_current[:state]
+        @arrayOfCurrentStates
     end 
 
     def show 
         @location = Location.find(params[:id])
     end 
+
+    private
+
+    def states_current #arguemnt?
+        url = "https://covidtracking.com/api/states"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfCurrentStates = workable_hash["items"]
+    end
 end

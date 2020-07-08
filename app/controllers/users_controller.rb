992e5b78cff5c1cb0e248303a19c834bc9461e03
travisprol0 <travisjohnprol@gmail.com>
2020-07-08 12:41:56 -0400

    user show done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find(params[:id])
+        @location = Location.find(params[:id])
+        @favorites = Favorite.all
     end 
 
     def create

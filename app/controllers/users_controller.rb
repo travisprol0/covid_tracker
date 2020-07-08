209a5076ec3d0e8cf36451be768b80f6b7410786
travisprol0 <travisjohnprol@gmail.com>
2020-07-08 14:09:57 -0400

    did not do this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,12 @@ class UsersController < ApplicationController
         end 
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.delete
+        redirect_to users_path
+    end
+
 
     private
 

540648be76ee8156ec5d9b3e5e456eb0c3c74e40
travisprol0 <travisjohnprol@gmail.com>
2020-07-09 11:50:33 -0400

    fixed me breaking the whole thing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :favorites 
     has_many :locations, through: :favorites
     validates :username, :first_name, :last_name, presence: true 
-    validates :username, uniqueness: true 
+    
 
     def to_s
         self.first_name + " " + self.last_name 
